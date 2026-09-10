f77e5a4bcbcf5000153abb6683c2d34f581478bd
st1vv <s.ivanov@scroogefrog.com>
2026-09-10 11:37:10 +0300

    added homepage and react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { defineConfig } from 'vite'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
